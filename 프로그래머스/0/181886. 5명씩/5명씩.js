b9c235863bbe8def0e8b56fc14d84d979d6ce91c
hongdul <48382951+hongdul@users.noreply.github.com>
2025-05-26 19:54:15 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -0,0 +1,9 @@
+function solution(names) {
+    var answer = [];
+    
+    for (var i = 0; i < names.length; i++) {
+        if (i % 5 == 0) answer.push(names[i]);
+    }
+    
+    return answer;
+}
